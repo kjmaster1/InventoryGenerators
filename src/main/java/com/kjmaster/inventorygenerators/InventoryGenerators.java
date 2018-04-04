a6e380c26a7d593b6a22df40d04e6eb47c38f775
kjmaster1 <kurtis.clements@hotmail.com>
2018-04-04 15:26:16 +0100

    Fix Dependencies

--- src/main/java/com/kjmaster/inventorygenerators/InventoryGenerators.java
+++ src/main/java/com/kjmaster/inventorygenerators/InventoryGenerators.java
@@ -18,8 +18,8 @@ public class InventoryGenerators
 {
     public static final String MODID = "inventorygenerators";
     public static final String NAME = "Inventory Generators";
-    public static final String VERSION = "1.0.0";
-    public static final String DEPENDENCIES = "required-after:tconstruct;required-after:actuallyadditions;required-after:kjlib";
+    public static final String VERSION = "1.0.1";
+    public static final String DEPENDENCIES = "after:tconstruct;after:actuallyadditions;required-after:kjlib";
     public static Logger LOGGER;
 
     @SidedProxy(clientSide = "com.kjmaster.inventorygenerators.client.ClientProxy", serverSide = "com.kjmaster.inventorygenerators.common.CommonProxy")
