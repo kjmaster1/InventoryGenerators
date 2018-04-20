b4d6b6662532a9890a6abfe85a5b917d7993a3f1
kjmaster1 <kurtis.clements@hotmail.com>
2018-04-20 20:38:11 +0100

    1.1.1

--- src/main/java/com/kjmaster/inventorygenerators/InventoryGenerators.java
+++ src/main/java/com/kjmaster/inventorygenerators/InventoryGenerators.java
@@ -21,7 +21,7 @@ public class InventoryGenerators
 {
     public static final String MODID = "inventorygenerators";
     public static final String NAME = "Inventory Generators";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.1.1";
     public static final String DEPENDENCIES = "after:tconstruct;after:actuallyadditions;required-after:kjlib";
     public static Logger LOGGER;
 
